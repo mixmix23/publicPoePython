6ec392be9c264ac72ed1c19ee8fae37ad1732aec
Eric <mixmix23@gmail.com>
2023-03-22 20:33:13 -0500

    from Those

--- Streamlit_TFT.py
+++ Streamlit_TFT.py
@@ -28,7 +28,8 @@ if response.status_code == 200:
     date_str = dt_central.strftime("%Y-%m-%d %H:%M:%S %Z")
 
     st.write("Timestamp: %s" % date_str)
-    st.dataframe(df, width=300, height=700)
+    # st.dataframe(df, width=300, height=700)
+    st.dataframe(df, use_container_width=True, height=700)
 
 else:
     print(f"Failed to retrieve {url}. Status code: {response.status_code}")
