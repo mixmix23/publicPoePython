c4ca1219a490fca6f09d37d684b10f37cedcdef4
Eric <mixmix23@gmail.com>
2023-03-22 20:01:03 -0500

    from Those

--- Streamlit_TFT.py
+++ Streamlit_TFT.py
@@ -5,9 +5,10 @@ import pandas as pd
 
 select_tft_json = st.sidebar.selectbox(
     "Select JSON",
-    ("bulk-beasts", "bulk-breach", "bulk-compasses")
+    ("bulk-beasts", "bulk-breach", "bulk-compasses", "bulk-expedition", "bulk-heist", "bulk-invitation", "bulk-legion-jewels",
+     "bulk-lifeforce", "bulk-maps", "bulk-sets", "bulk-simulacrum", "bulk-stacked-deck", "bulk-vessel", "bulk-watcher's-eye",
+     "hideout", "service")
 )
-
 
 url = f"https://raw.githubusercontent.com/The-Forbidden-Trove/tft-data-prices/master/lsc/{select_tft_json}.json"
 response = requests.get(url)
