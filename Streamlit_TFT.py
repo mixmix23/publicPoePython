a28951a60ee38b7170ec71fc84bb3820c95a5c21
Eric <mixmix23@gmail.com>
2023-03-22 20:16:17 -0500

    from Those

--- Streamlit_TFT.py
+++ Streamlit_TFT.py
@@ -17,8 +17,8 @@ if response.status_code == 200:
     data = response.json()
     df = pd.DataFrame(data['data'])
     epoch_time = data['timestamp']
-    real_time = datetime.datetime.utcfromtimestamp(epoch_time).strftime('%Y-%m-%d %H:%M:%S')
-    st.write("Timestamp: %s" % real_time)
+
+    st.write("Timestamp: %s" % epoch_time)
     st.dataframe(df)
     # Process the JSON data here
 else:
